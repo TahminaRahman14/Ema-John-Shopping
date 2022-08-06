37cd98f9207a92796b7bde0ddfbe5433ad3d8f86
Tahmina Rahman <tahminarahmansurovi@gmail.com>
2022-08-07 03:24:11 +0600

    added react route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import Shop from "./components/Shop/Shop";
 import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Shop />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/orders" element={<Orders />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
